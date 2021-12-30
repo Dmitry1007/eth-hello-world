78624750ea8b861f3815649b155260782136b49c
Dmitry Sky <dmitry.sky@icloud.com>
2021-12-30 11:02:17 -0700

    Add MIT license identifier, compile code

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,9 +1,10 @@
-pragma solidity >=0.5.8 <0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.8 <0.9.0;
 
 contract HelloWorld {
    string public message;
 
-   constructor(string memory initMessage) public {
+   constructor(string memory initMessage) {
        message = initMessage;
    }
 
